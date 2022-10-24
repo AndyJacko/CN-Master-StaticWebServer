811bd4545bf8bfc375ee2047ea4efdfe5527b112
Andy Jacko <wisdom.mcr@gmail.com>
2022-10-24 11:50:31 +0100

    Add About and Contact pages...

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use("/static", express.static("public"));
+app.use("/", express.static("public"));
 
 app.listen(3000, () => {
   console.log("Server Connected...");
